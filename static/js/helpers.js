a68e00c374e330f90519b89a5b6c02de1567b027
Bryce <121913194+hackdaddy8000@users.noreply.github.com>
2023-01-29 08:51:48 -0500

    init

---
+++ static/js/helpers.js
@@ -0,0 +1,108 @@
+function emotion2SsmlStyle(emotion) {
+    var style;
+    if (emotion == null) {
+        style = "General";
+    } else {
+        const emotionToSsmlStyleMap = {
+            "neutral": "General",
+            "anger": "Angry",
+            "disgust": "Unfriendly",
+            "fear": "Terrified",
+            "joy": "Excited",
+            "sadness": "Sad",
+            "surprise": "Excited"
+        };
+        style = emotionToSsmlStyleMap[emotion]
+    }
+    return style;
+}
+
+/**
+ * https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/speech-synthesis-markup
+ * SSML stands for Speech Synthesis Markup Language. It is used with Azure's text to speech API to define
+ * aspects about how you want the speech output to be like. For example, there are different voices (Jenny, Jane, John, etc).
+ * You can use SSML to choose which voice. This helper method just formats certain variables into a proper SSML string.
+ * @param {string} response What you want her to say
+ * @param {string} name Voice name (see Azure demo)
+ * @param {string} style Voice style (see Azure demo) 
+ * @returns 
+ */
+function createSsml(response, name, emotion) {
+    var style = emotion2SsmlStyle(emotion);
+    return `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">
+                    <voice name="en-US-JennyNeural">
+                        <mstts:viseme type="FacialExpression"/>
+                        <mstts:express-as style="${style}" >
+                            <prosody rate="17%" pitch="7%">
+                                ${response}
+                            </prosody>
+                        </mstts:express-as>
+                    </voice>
+                </speak>`;
+}
+
+/**
+ * In the URL, there are parameters ex: https://hackdaddy.dev?PARAM_NAME=PARAM_VALUE.
+ * This function helps get those params.
+ * @param {string} key PARAM_NAME 
+ * @returns PARAM_VALUE
+ */
+function getURLParam(key) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get(key)
+}
+
+/**
+ * https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/how-to-speech-synthesis-viseme?tabs=visemeid&pivots=programming-language-javascript#map-phonemes-to-visemes
+ * The Azure TTS API tells us what the shape of the mouth should be at certain points in time. That shape is called a viseme.
+ * This function takes the Waifu model and the viseme and makes her mouth that shape.
+ * @param {*} model Waifu model
+ * @param {*} v visemeID
+ */
+function setViseme(model, v) {
+    const visemeMap = [[1, 0], [1, 1], [1, 1], [.3, .7], [1, .3], [1, .3], [1, .1], [.1, .1], [.3, .5], [1, .8], [.2, 2], [1, 1], [1, .2], [.3, .3], [.9, .2], [1, .1], [.1, .1], [1, .3], [1, .05], [1, .3], [1, .6], [1, 0]];
+    model.internalModel.coreModel.setParamFloat('PARAM_MOUTH_OPEN_Y', visemeMap[v][1] ?? 0);
+}
+
+/**
+ * 
+ * @param {string} emotion output from text2emotion
+ * @returns index of the appropriate expression for the emotion
+ */
+function emotion2ModelExpression(emotion) {
+    const emotionMap = {
+        "neutral": 0,
+        "anger": 1,
+        "disgust": 2,
+        "fear": 3,
+        "joy": 4,
+        "sadness": 5,
+        "surprise": 6
+    };
+    return emotionMap[emotion];
+}
+
+function openAIAPICompletionReq(key, prompt, callback) {
+    $.ajax({
+        url: 'https://api.openai.com/v1/completions',
+        type: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + key
+        },
+        data: JSON.stringify({
+            'model': 'text-davinci-003',
+            'prompt': prompt,
+            'temperature': 0.8,
+            'max_tokens': 70
+        }),
+        success: function (data) {
+            callback(data.choices[0].text);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+            callback(null);
+        },
+    });
+}
---
+++ static/js/main.js
@@ -0,0 +1,181 @@
+const modelURL =
+    "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/shizuku/shizuku.model.json";
+
+/**
+ * Siri sound. There is a delay between when you tap and when it starts transcripting. 
+ * This sound helps make people wait for it to start transcripting.
+*/
+const startListeningAudioElement = new Audio("static/media/startListening.mp3");
+
+var $name_label, $transcription;
+
+var disabled = true;
+
+const username = getURLParam("username") || "hackdaddy8000";
+const waifuName = getURLParam("name") || "Goku 3";
+const promptBase = getURLParam("prompt")
+    || `You are a smug girl. You enjoy belittling people. 
+    You love pointing out how much smarter you are than everyone. 
+    You are currently hiding in your basement from the feds. 
+    The feds are after you due to various cyber crimes. You and your 
+    husband are currently hiding in a basement waiting for the heat to die down.`;
+
+const openAIKey = getURLParam("openai");
+if (openAIKey == null) {
+    alert("No OpenAI API key provided");
+}
+
+const subscriptionKey = getURLParam("speech_key");
+const serviceRegion = getURLParam("speech_region");
+if (subscriptionKey == null || serviceRegion == null) {
+    alert("No Azure speech key or service region");
+}
+const speechConfig = SpeechSDK.SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
+speechConfig.speechRecognitionLanguage = "en-US";
+const audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+
+const nyckelKey = getURLParam("nyckel");
+
+var recognizer;
+var synthesizer;
+
+const interactions = [];
+function buildPrompt(newPrompt) {
+    return promptBase + interactions.join("\n") + "\nMe:" + newPrompt + "\nYou: ";
+}
+function pushInteraction(prompt, response) {
+    if (interactions.length > 12) {
+        interactions.shift();
+    }
+    interactions.push(`Me: ${prompt}\nYou: ${response}`)
+}
+
+function tap(onStartListening, onLoading, onResults, onStartTalking) {
+    if (disabled) {
+        return;
+    } else {
+        disabled = true;
+    }
+    startListeningAudioElement.play();
+    $transcription.text("Listening...");
+    $name_label.text(username + ":");
+
+    onStartListening();
+
+    synthesizer = new SpeechSDK.SpeechSynthesizer(speechConfig);
+
+    const visemeAcc = [];
+    synthesizer.visemeReceived = function (s, e) {
+        visemeAcc.push(e);
+    };
+
+    recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+    recognizer.recognizing = (s, e) => {
+        $transcription.text(e.result.text);
+    };
+
+    // Start listening
+    recognizer.recognizeOnceAsync(
+        function (result) {
+            onLoading()
+            $transcription.text(result.text);
+
+            var prompt = buildPrompt(result.text);
+
+            // Get response from openAI
+            openAIAPICompletionReq(openAIKey, prompt, function (response) {
+                pushInteraction(result.text, response);
+                disabled = false;
+
+                $name_label.text(waifuName);
+                $transcription.text(response);
+
+                var emotion = "neutral";
+                var data = {
+                    "response": response,
+                    "emotion": emotion,
+                };
+                onResults(data);
+
+                var ssml = createSsml(response, "en-US-JennyNeural", emotion);
+                // Text to speech
+                synthesizer.speakSsmlAsync(
+                    ssml,
+                    function (result) {
+                        if (result.reason === SpeechSDK.ResultReason.Canceled) {
+                            console.log("synthesis failed. Error detail: " + result.errorDetails + "\n");
+                        }
+                        onStartTalking(visemeAcc);
+                        synthesizer.close();
+                        synthesizer = undefined;
+                    },
+                    function (err) {
+                        window.console.log(err);
+                        synthesizer.close();
+                        synthesizer = undefined;
+                    });
+            });
+
+            recognizer.close();
+            recognizer = undefined;
+        },
+        function (err) {
+            disabled = false;
+            $transcription.text(err);
+            window.console.log(err);
+
+            recognizer.close();
+            recognizer = undefined;
+        });
+};
+
+(async function main() {
+    $('.overlay').delay(1500).fadeOut(800);
+
+    $name_label = $('#nametag');
+    $transcription = $('#transcription');
+
+    if (!!window.SpeechSDK) {
+        SpeechSDK = window.SpeechSDK;
+        disabled = false;
+        console.log("Speech enabled");
+    }
+
+    // Waifu stuff
+
+    const app = new PIXI.Application({
+        view: document.getElementById("canvas"),
+        autoStart: true,
+        resizeTo: window
+    });
+
+    const model = await PIXI.live2d.Live2DModel.from(modelURL);
+
+    model.internalModel.motionManager.groups.idle = 'Idle';
+
+    app.stage.addChild(model);
+
+    model.scale.set(0.5);
+    model.x = -100;
+
+    function tapImpl() {
+        tap(function () {
+            model.internalModel.motionManager.expressionManager.setExpression(0);
+        }, function () {
+
+        }, function (data) {
+            var emotion = data["emotion"];
+            model.internalModel.motionManager.expressionManager.setExpression(emotion2ModelExpression(emotion));
+        }, function (visemes) {
+            // Makes her mouth move according to the visemes
+            var start = Date.now();
+            for (let e of visemes) {
+                setTimeout(() => {
+                    setViseme(model, e.visemeId)
+                }, e.audioOffset / 10000 - (Date.now() - start));
+            }
+        })
+    }
+
+    $transcription.click(tapImpl)
+})();
